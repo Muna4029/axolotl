74327582757ff0b45565a8c7e02052ec9f634f7f
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-12 11:39:57 -0400

    Muna4029__diff__id_490

--- src/setuptools_axolotl_dynamic_dependencies.py
+++ src/setuptools_axolotl_dynamic_dependencies.py
@@ -58,7 +58,11 @@ def parse_requirements():
             else:
                 raise ValueError("Invalid version format")
 
-            if (major, minor) >= (2, 5):
+            if (major, minor) >= (2, 7):
+                _install_requires.pop(_install_requires.index(xformers_version))
+                _install_requires.append("xformers==0.0.31.post1")
+                _install_requires.pop(_install_requires.index(autoawq_version))
+            elif (major, minor) >= (2, 5):
                 _install_requires.pop(_install_requires.index(xformers_version))
                 if patch == 0:
                     _install_requires.append("xformers==0.0.28.post2")
