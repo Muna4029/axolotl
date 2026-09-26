993a5be16a09b26615192c752319a2e0b53451f4
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-25 21:32:24 -0400

    Muna4029__diff__id_485

--- src/setuptools_axolotl_dynamic_dependencies.py
+++ src/setuptools_axolotl_dynamic_dependencies.py
@@ -58,7 +58,12 @@ def parse_requirements():
             else:
                 raise ValueError("Invalid version format")
 
-            if (major, minor) >= (2, 5):
+            # Handle torch >= 2.6 first (more specific)
+            if (major, minor) >= (2, 6):
+                _install_requires.pop(_install_requires.index(xformers_version))
+                _install_requires.append("xformers==0.0.28.post3")
+                _install_requires.pop(_install_requires.index(autoawq_version))
+            elif (major, minor) >= (2, 5):
                 _install_requires.pop(_install_requires.index(xformers_version))
                 if patch == 0:
                     _install_requires.append("xformers==0.0.28.post2")
